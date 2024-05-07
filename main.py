e51a85a09aa72f2c859058bde43d0010cebb22e0
Dan-JV <dan.johansson@visma.com>
2024-05-07 15:16:34 +0200

    main chunker

--- main.py
+++ main.py
@@ -18,21 +18,22 @@ def load_data(file_path):
 
 
 def split_page(files):
+    # counter for page
+    i = 0
     for file in files:
-        # counter for page
-        i = 0
+        
+        # Counter for chunks
+        j = 0
 
         page = load_data(file)     
 
         chunks = markdown_splitter.split_text(page)
 
-        url = chunks[0]
-
-        print(len(chunks))
+        url = chunks[0].page_content
 
         for chunk in chunks:
             # Counter for chunks
-            j = 0
+            
 
             try:
                 # First chunk is the url
@@ -40,13 +41,17 @@ def split_page(files):
             except:
                 print(f"Failed to add url to chuck for {file}")
 
-            json_chunk = json.loads(chunk.json())
+            chunk_dict = {"metadata": chunk.metadata, "content": chunk.page_content}
 
             # Assuming `chunk` is a Python dictionary that you want to serialize into JSON
             output_file_path = f'data/chunks/chunk_{i}_{j}.json'  # Changed file extension to .json
             with open(output_file_path, 'w', encoding='utf-8') as output_file:
-                json.dump(json_chunk, output_file)  # Serializes `chunk` to a JSON formatted string and writes it to `output_file`
+                json.dump(chunk_dict, output_file)  # Serializes `chunk` to a JSON formatted string and writes it to `output_file`
 
+            j += 1
+        print(f"chunked page {i} sucessfull")
+        i += 1
+            
 
     return chunks
 
