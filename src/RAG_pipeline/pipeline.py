3c340b2990418b718f9358c205b6900dffc74826
Dan <dan.johansson@visma.com>
2024-05-16 10:08:44 +0200

    Change search method

--- src/RAG_pipeline/pipeline.py
+++ src/RAG_pipeline/pipeline.py
@@ -28,9 +28,9 @@ def create_pipeline():
     collection_name = "text-titan-embed-text-v1"
     qdrant_client = initialize_qdrant_client(collection_name, embeddings_model)
 
-    retriever = qdrant_client.as_retriever(
-        search_type="mmr", search_kwargs={"k": 4, "fetch_k": 30}
-    )
+
+    #search_type="mmr", search_kwargs={"k": 10, "fetch_k": 50}
+    retriever = qdrant_client.as_retriever()
     prompt = PromptTemplate.from_template(
         "As an expert system designed to assist with inquiries about Visma Enterprise A/S's internal guides, you are to provide answers that are strictly based on the content of these guides.\nUse specific terminology and keywords from the guides to maintain consistency with Visma’s standards.\nMaintain a professional tone throughout your response and ensure that the information is both comprehensive and precise.\nIf applicable, reference specific sections or points within the guides to add clarity and relevance to your answer.\nAll responses must be provided in Danish.\nIf a query is vague or you do not have sufficient information to provide an accurate response, ask for clarification or admit that you do not know the answer.\nAvoid providing speculative information or details outside the scope of the internal guides.\n\n<context>\n{context}\n</context>\n\nUser question:{input}"
     )
