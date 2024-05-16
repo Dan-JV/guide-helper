9be1dba89cacd49b68bc9177b60f6f717f3fa639
Dan <dan.johansson@visma.com>
2024-05-16 14:47:29 +0200

    Add retrieved context drop drown debugger

--- app.py
+++ app.py
@@ -13,17 +13,26 @@ def app():
     os.environ["AWS_ACCESS_KEY_ID"] = st.secrets["AWS_ACCESS_KEY_ID"]
     os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets["AWS_SECRET_ACCESS_KEY"]
 
-
     retrieval_chain = pipeline.create_pipeline()
 
     st.title("Guide Helper")
 
+    # Session state variables
+
     if "messages" not in st.session_state:
         st.session_state.messages = []
+
+    if "contexts" not in st.session_state:
+        st.session_state.contexts = []
+
+    if "selected_context_index" not in st.session_state:
+        st.session_state.selected_context_index = None
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+
+    # Start of query loop
 
     if prompt := st.chat_input("How can I assist you?"):
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -41,9 +50,37 @@ def app():
                 }
             )
 
-            response = st.write(messages["answer"])
+            response = messages["answer"]
+            st.write(response)
+
+            # Extract and store context in session state
+            st.session_state.contexts = messages.get("context", [])
+            st.session_state.selected_context_index = 0  # Reset index when new context is added
+
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+    
+    # Debugger: This create a drop down menu to show the retrieved context objects
+    if st.session_state.contexts:
+        # Created a selectbox to select the retrieved context objects
+        selected_index = st.selectbox(
+            "Context objects:",
+            range(len(st.session_state.contexts)),
+            format_func=lambda x: f"Retrieved context #{x + 1}",
+            index=None
+        )
+
+        # Update session state only if the selection changes
+        if selected_index != st.session_state.selected_context_index:
+            st.session_state.selected_context_index = selected_index
+
+        # Only show context details if an item is selected
+        if st.session_state.selected_context_index is not None:
+            # Get the selected context object and display its metadata and content
+            selected_context = st.session_state.contexts[st.session_state.selected_context_index]
+            st.json(body=selected_context.metadata)
+            st.markdown(body=selected_context.page_content)
 
 if __name__ == '__main__':
     load_dotenv()
-    app() 
+    app()
