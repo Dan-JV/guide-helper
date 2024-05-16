167b010f978d2a93422140d6ab977b9f7fa90537
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-16 14:50:11 +0200

    pipeline

--- src/RAG_pipeline/pipeline.py
+++ src/RAG_pipeline/pipeline.py
@@ -12,7 +12,7 @@ from src.RAG_pipeline.utils import initialize_qdrant_client
 
 def create_pipeline():
 
-    model_id = "anthropic.claude-instant-v1"
+    model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
     bedrock_runtime_client = boto3.client(
         service_name="bedrock-runtime",
         region_name="us-east-1",
@@ -28,8 +28,6 @@ def create_pipeline():
     collection_name = "text-titan-embed-text-v1"
     qdrant_client = initialize_qdrant_client(collection_name, embeddings_model)
 
-
-    #search_type="mmr", search_kwargs={"k": 10, "fetch_k": 50}
     retriever = qdrant_client.as_retriever()
     prompt = PromptTemplate.from_template(
         "As an expert system designed to assist with inquiries about Visma Enterprise A/S's internal guides, you are to provide answers that are strictly based on the content of these guides.\nUse specific terminology and keywords from the guides to maintain consistency with Visma’s standards.\nMaintain a professional tone throughout your response and ensure that the information is both comprehensive and precise.\nIf applicable, reference specific sections or points within the guides to add clarity and relevance to your answer.\nAll responses must be provided in Danish.\nIf a query is vague or you do not have sufficient information to provide an accurate response, ask for clarification or admit that you do not know the answer.\nAvoid providing speculative information or details outside the scope of the internal guides.\n\n<context>\n{context}\n</context>\n\nUser question:{input}"
--- src/RAG_pipeline/utils.py
+++ src/RAG_pipeline/utils.py
@@ -1,12 +1,30 @@
 import os
+from typing import Any
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from langchain_community.vectorstores import Qdrant
 from qdrant_client import QdrantClient
 
 
 def initialize_qdrant_client(collection_name, embeddings_model):
-    client = QdrantClient(os.getenv("QDRANT_ENDPOINT"), api_key=os.getenv("QDRANT_API_KEY"))
+    client = QdrantClient(
+        os.getenv("QDRANT_ENDPOINT"), api_key=os.getenv("QDRANT_API_KEY")
+    )
     qdrant_client = Qdrant(client, collection_name, embeddings=embeddings_model)
 
-
     return qdrant_client
+
+
+def process_template(template_file: str, data: dict[str, Any]) -> str:
+    """Process a Jinja template file with the provided data.
+    Example:
+    ```
+    process_template("prompts/guide_helper.jinja", {"context": "context", "input": "input"})
+    ```
+    """
+    jinja_env = Environment(
+        loader=FileSystemLoader(searchpath="./"), autoescape=select_autoescape()
+    )
+    template = jinja_env.get_template(template_file)
+    return template.render(**data)
+
