a1483337f85f4c8a4249576aff4ce97f45c14ad9
Dan <dan.johansson@visma.com>
2024-05-17 09:20:15 +0200

    Implemented feed back from and removed input memory

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import requests
 import streamlit as st
 from dotenv import load_dotenv
 
@@ -28,38 +28,79 @@ def app():
     if "selected_context_index" not in st.session_state:
         st.session_state.selected_context_index = None
 
+
+    if "feedback" not in st.session_state:
+        st.session_state.feedback = []
+
+    if "thumbs_feedback" not in st.session_state:
+        st.session_state.thumbs_feedback = None
+
+    # -------------------------------------------#
+
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
     # Start of query loop
 
-    if prompt := st.chat_input("How can I assist you?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    if input := st.chat_input("How can I assist you?"):
+        st.session_state.messages.append({"role": "user", "content": input})
 
         with st.chat_message("user"):
-            st.markdown(prompt)
+            st.markdown(input)
 
+        # TODO: we are curently invoking the chain using all the messages in the messages history?? we should not have this behavior
         with st.chat_message("assistant"):
-            messages_string = "\n".join(
-                [f"{m['role']}: {m['content']}" for m in st.session_state.messages]
-            )
-            messages = retrieval_chain.invoke(
+            
+            chain_output = retrieval_chain.invoke(
                 {
-                    "input": messages_string,
+                    "input": input,
                 }
             )
 
-            response = messages["answer"]
+            response = chain_output["answer"]
             st.write(response)
 
             # Extract and store context in session state
-            st.session_state.contexts = messages.get("context", [])
+            st.session_state.contexts = chain_output.get("context", [])
             st.session_state.selected_context_index = 0  # Reset index when new context is added
 
         st.session_state.messages.append({"role": "assistant", "content": response})
 
     
+    # Thumbs up and thumbs down buttons
+    if st.session_state.contexts:
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button('👍'):
+                st.session_state.thumbs_feedback = 'good'
+        with col2:
+            if st.button('👎'):
+                st.session_state.thumbs_feedback = 'bad'
+
+        # Feedback form
+        with st.form(key='feedback_form'):
+            email = st.text_input("Email")
+            comment = st.text_area("Leave a comment")
+            submitted = st.form_submit_button(label='Submit')
+
+            if submitted:
+                # Feedback form dict
+                feedback ={
+                    'message_history': st.session_state.messages,
+                    'feedback': st.session_state.thumbs_feedback,
+                    'email' : email,
+                    'comment': comment,
+                }
+                st.session_state.thumbs_feedback = None
+
+                # Post the feedback to the Pipedream endpoint
+                feedback_endpoint_response = requests.post("https://eovyzv364l93k2e.m.pipedream.net", json=feedback)
+                if feedback_endpoint_response.status_code == 200:
+                    st.success("Feedback submitted successfully!")
+                else:
+                    st.error("Failed to submit feedback.")
+
     # Debugger: This create a drop down menu to show the retrieved context objects
     if st.session_state.contexts:
         # Created a selectbox to select the retrieved context objects
