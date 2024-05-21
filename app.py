19a7da60b781dd643711d0d7d476b602cbed64f7
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-21 13:08:32 +0200

    weave

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ import os
 
 if __name__ == "__main__":
     config = load_config()
-    wandb.login(key=os.getenv("WEAVE_API_KEY"))
+    wandb.login(key=os.getenv("WEAVE_API_KEY"), verify=True)
     weave.init(project_name="Guide Helper")
     app(config)
 
