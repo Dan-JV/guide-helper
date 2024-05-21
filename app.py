65e9543e73c64c4c653932eb6d01f47e5996a87d
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-21 08:46:25 +0200

    new retriever

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 import os
+
 import requests
 import streamlit as st
-from dotenv import load_dotenv
+import weave
+from dotenv import dotenv_values
+from langchain_core.messages import AIMessage, HumanMessage
+
 
 from src.RAG_pipeline import pipeline
 
 
-def app():
+@weave.op() # logs input and output of calls 
+def invoke_chain_helper_fn(chain, input, chat_history):
+    
+    chain_response =  chain.invoke(
+        {
+            "input": input,
+            "chat_history": chat_history
+        }
+    )
+    answer = chain_response["answer"]
+    chat_history.extend([HumanMessage(content=input), AIMessage(content=answer)])
 
-    os.environ["QDRANT_API_KEY"] = st.secrets["QDRANT_API_KEY"]
-    os.environ["QDRANT_ENDPOINT"] = st.secrets["QDRANT_ENDPOINT"]
-    os.environ["AWS_ACCESS_KEY_ID"] = st.secrets["AWS_ACCESS_KEY_ID"]
-    os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets["AWS_SECRET_ACCESS_KEY"]
+    return chain_response, answer, chat_history
 
-    retrieval_chain = pipeline.create_pipeline()
 
-    st.title("Guide Helper")
-
-    # Session state variables
-
+def initialize_streamlit_session_states():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
@@ -33,84 +40,115 @@ def app():
 
     if "thumbs_feedback" not in st.session_state:
         st.session_state.thumbs_feedback = None
-
-    # -------------------------------------------#
-
+    
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+
+
+def start_streamlit_app():
+    st.title("Guide Helper")
+    initialize_streamlit_session_states()
+
+
+@weave.op()
+def send_feedback(feedback):
+    # Post the feedback to the Pipedream endpoint
+    feedback_endpoint_response = requests.post(
+        "https://eovyzv364l93k2e.m.pipedream.net", json=feedback
+    )
+
+    return feedback_endpoint_response
+
+
+def feedback():
+    
+    # Thumbs up/down feedback button
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        if st.button("👍"):
+            st.session_state.thumbs_feedback = "good"
+    with col2:
+        if st.button("👎"):
+            st.session_state.thumbs_feedback = "bad"
+
+    # Feedback form
+    with st.form(key="feedback_form"):
+        # Populate the feedback form
+        name = st.text_input("Name")
+        email = st.text_input("Email")
+        comment = st.text_area("Leave a comment")
+
+        # Submit button
+        submitted = st.form_submit_button(label="Submit")
+
+        if submitted:
+            # Feedback form dict
+            feedback = {
+                "message_history": st.session_state.Q_A,
+                "feedback": st.session_state.thumbs_feedback,
+                "name": name,
+                "email": email,
+                "comment": comment,
+                "url": [
+                    chunk.metadata.get("url") for chunk in st.session_state.contexts
+                ],
+            }
+            st.session_state.thumbs_feedback = None
+
+            feedback_endpoint_response = send_feedback(feedback)
+
+            if feedback_endpoint_response.status_code == 200:
+                st.success("Feedback submitted successfully!")
+            else:
+                st.error("Failed to submit feedback.")
+
+
+
+
+def app():
+
+    chat_history = []
+    retrieval_chain = pipeline.create_pipeline()
+
+
+    start_streamlit_app() # title and session state variables
+
+    # -------------------------------------------#
+
 
     # Start of query loop
 
     if input := st.chat_input("How can I assist you?"):
         st.session_state.messages.append({"role": "user", "content": input})
 
-        with st.chat_message("user"):
-            st.markdown(input)
+        st.chat_message("user").write(input)
 
-        with st.chat_message("assistant"):
-            
-            chain_output = retrieval_chain.invoke(
-                {
-                    "input": input,
-                }
-            )
+        chain_response, answer, chat_history = invoke_chain_helper_fn(retrieval_chain, input, chat_history)
+        context = chain_response["context"]
 
-            response = chain_output["answer"]
-            st.write(response)
+        #st.markdown(answer) # write ai response
+        st.chat_message("assistant").write(answer)
 
-            # Temporary Q&A list to store user input and assistant response which gets posted to Airtable
-            # TODO This should be removed and handled in a better way
-            st.session_state.Q_A = [{"role": "user", "content": input},{"role": "assistant", "content": response}]
+        # Temporary Q&A list to store user input and assistant response which gets posted to Airtable
+        # TODO This should be removed and handled in a better way TODO: Solved?
+        st.session_state.Q_A = [
+            {"role": "user", "content": input},
+            {"role": "assistant", "content": answer},
+        ]
 
-            # Extract and store context in session state
-            st.session_state.contexts = chain_output.get("context", [])
-            st.session_state.selected_context_index = 0  # Reset index when new context is added
+        # Extract and store context in session state
+        st.session_state.contexts = context
+        st.session_state.selected_context_index = (
+            0  # Reset index when new context is added
+        )
 
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages.append({"role": "assistant", "content": answer})
 
     # Check if the is a question and answer object in session state
     if st.session_state.Q_A:
-
-        # Thumbs up/down feedback button
-        col1, col2 = st.columns([1, 1])
-        with col1:
-            if st.button('👍'):
-                st.session_state.thumbs_feedback = 'good'
-        with col2:
-            if st.button('👎'):
-                st.session_state.thumbs_feedback = 'bad'
-
-        # Feedback form
-        with st.form(key='feedback_form'):
-
-            # Populate the feedback form
-            name = st.text_input("Name")
-            email = st.text_input("Email")
-            comment = st.text_area("Leave a comment")
-
-            # Submit button
-            submitted = st.form_submit_button(label='Submit')
-
-            if submitted:
-                # Feedback form dict
-                feedback ={
-                    'message_history': st.session_state.Q_A,
-                    'feedback': st.session_state.thumbs_feedback,
-                    'name' : name, 
-                    'email' : email,
-                    'comment': comment,
-                    'url' : [chunk.metadata.get("url") for chunk in st.session_state.contexts]
-                }
-                st.session_state.thumbs_feedback = None
-
-                # Post the feedback to the Pipedream endpoint
-                feedback_endpoint_response = requests.post("https://eovyzv364l93k2e.m.pipedream.net", json=feedback)
-                if feedback_endpoint_response.status_code == 200:
-                    st.success("Feedback submitted successfully!")
-                else:
-                    st.error("Failed to submit feedback.")
-
+        feedback()
+        
     # Debugger: This create a drop down menu to show the retrieved context objects
     if st.session_state.contexts:
         # Created a selectbox to select the retrieved context objects
@@ -118,7 +156,7 @@ def app():
             "Context objects:",
             range(len(st.session_state.contexts)),
             format_func=lambda x: f"Retrieved context #{x + 1}",
-            index=None
+            index=None,
         )
 
         # Update session state only if the selection changes
@@ -128,10 +166,24 @@ def app():
         # Only show context details if an item is selected
         if st.session_state.selected_context_index is not None:
             # Get the selected context object and display its metadata and content
-            selected_context = st.session_state.contexts[st.session_state.selected_context_index]
+            selected_context = st.session_state.contexts[
+                st.session_state.selected_context_index
+            ]
             st.json(body=selected_context.metadata)
             st.markdown(body=selected_context.page_content)
 
-if __name__ == '__main__':
-    load_dotenv()
+def set_environment_variables():
+    keys = dict(**dotenv_values(".env"))  # Load all the environment variables from a .env file
+    
+    # Automatically load all keys from the .env file into the environment
+    for key in keys:
+        os.environ[key] = st.secrets[key]
+
+
+if __name__ == "__main__":
+    set_environment_variables()
+    
+
+    weave.init(project_name="Guide Helper")
     app()
+
