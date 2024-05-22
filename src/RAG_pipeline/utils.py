86302c52fd2c24979b12795f317bba9222519685
lundberg-johan12 <johan.lundberg@visma.com>
2024-05-22 13:36:26 +0200

    conf

--- src/RAG_pipeline/utils.py
+++ src/RAG_pipeline/utils.py
@@ -1,14 +1,14 @@
 import os
 from typing import Any
 
-from jinja2 import BaseLoader, Environment
+from jinja2 import Environment
+from langchain_aws import BedrockEmbeddings
 from langchain_community.vectorstores import Qdrant
 from qdrant_client import QdrantClient
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-
-def initialize_qdrant_client(collection_name, embeddings_model):
+def initialize_qdrant_client(collection_name: str, embeddings_model: BedrockEmbeddings) -> Qdrant:
     client = QdrantClient(
         os.getenv("QDRANT_ENDPOINT"), api_key=os.getenv("QDRANT_API_KEY")
     )
@@ -21,7 +21,6 @@ def load_template(template_file_path):
     with open(template_file_path, "r") as template_file:
         template_string = template_file.read()
     return template_string
-
 
 
 def render_jinja_template(template_file: str, data: dict[str, Any]) -> str:
@@ -51,5 +50,3 @@ def fill_in_template(
     template += "{context}"
 
     return template
-
-
